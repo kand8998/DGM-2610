5e20e5d7f93a612f60cff7cf711451cbcbc0e3b8
Kaitlynn <kand8998@gmail.com>
2020-02-08 12:37:44 -0700

    Wait For Seconds Script Added

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/WaitForSeconds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaitTimer : MonoBehaviour
+{
+    public float waitTime = 3f;
+    public GameAction waitAction;
+
+    private void Start()
+    {
+        waitAction.action += StartTimer;
+    }
+
+    private void StartTimer()
+    {
+        StartCoroutine(WaiteCoroutine());
+    }
+
+    private IEnumerator WaiteCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+        Debug.Log("Time's Up");
+    }
+}
