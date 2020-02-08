3f5dc2063fc70d7e7d260c6f35a18f126e412758
Kaitlynn <kand8998@gmail.com>
2020-02-08 14:23:18 -0700

    Health Object Added

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/CollectableObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class CollectableObject : ScriptableObject
+{
+    public UnityAction<int> coinAction;
+    public int value = 10;
+
+    public void Spend()
+    {
+        coinAction?.Invoke(value);
+    }
+}
---
+++ Chomp Night of the Living Vedge/Assets/Scripts/ObjectCollection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ObjectCollection : ScriptableObject
+{
+    public List<CollectableObject> collectList;
+    
+    public void AddCoin(CollectableObject collectObj)
+    {
+        if (!collectList.Contains(collectObj))
+        {
+            collectList.Add(collectObj);
+        }
+    }
+
+    public void UseCoin()
+    {
+        collectList[0].Spend();
+        collectList.RemoveAt(0);
+    }
+
+    public void UseCoinWithValue(int value)
+    {
+        var number = 5;
+        for (var i = 0; i < collectList.Count; i++)
+        {
+            var tool = collectList[i];
+            if (tool.value > number)
+            {
+                tool.Spend();
+                collectList.Remove(tool);
+            }
+        }
+    }
+}
