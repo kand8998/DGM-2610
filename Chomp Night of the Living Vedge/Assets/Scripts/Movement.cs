e8c325aaabe9409c991b6861066d948fb0786c46
Kaitlyn <kand8998@gmail.com>
2020-02-10 18:29:31 -0700

    movement script copied over

--- Chomp Night of the Living Vedge/Assets/Scripts/Movement.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/Movement.cs
@@ -6,10 +6,12 @@ public class Movement : MonoBehaviour
     private Vector3 position;
     private CharacterController controller;
     
-    public float moveSpeed = 30f,  gravity = 6f, jumpSpeed = 30f;
+    public float moveSpeed = 10f, jumpSpeed = 30f, rotationSpeed = 100;
+    public float gravity = 9.81f;
+    
     private int jumpCount;
     public int jumpCountMax = 2;
-    
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -20,11 +22,15 @@ public class Movement : MonoBehaviour
         position.x = moveSpeed*Input.GetAxis("Horizontal");
         position.z = moveSpeed*Input.GetAxis("Vertical");
         position.y -= gravity;
-
+        
         if (controller.isGrounded)
         {
             position.y = 0;
             jumpCount = 0;
+            if (position  != Vector3.zero)
+            {
+                controller.transform.forward = position;
+            }
         }
         
         if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
@@ -32,7 +38,6 @@ public class Movement : MonoBehaviour
             position.y = jumpSpeed;
             jumpCount++;
         }
-        
         controller.Move(position*Time.deltaTime);
     }
 }
