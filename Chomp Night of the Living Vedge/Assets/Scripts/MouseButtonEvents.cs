6d356770f18f892ca349f4f1d514c2ad98d555d7
Kaitlyn <kand8998@gmail.com>
2020-02-10 18:45:27 -0700

    Mouse Button Events Added

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/MouseButtonEvents.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseButtonEvents : MonoBehaviour
+{
+    public UnityEvent mouseButtonDownEvent, mouseButtonUpEvent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseButtonDownEvent.Invoke();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseButtonUpEvent.Invoke();
+        }
+    }
+}
