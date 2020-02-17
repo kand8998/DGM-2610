3a6dd1e8ce8cf8cc7d3513376c8f6881c03a4f88
Kaitlyn <kand8998@gmail.com>
2020-02-17 12:01:03 -0700

    Scripts Transferred Over

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/In Progress/ItemPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+    public override void Interact()
+    {
+        base.Interact();
+        PickUp();
+    }
+
+    private void PickUp()
+    {
+        bool wasPickedup = InventoryBehaviour.instance.Add(item);
+        if (wasPickedup)
+            Destroy(gameObject);
+    }
+}
---
+++ Chomp Night of the Living Vedge/Assets/Scripts/In Progress/MouseCast.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+
+public class MouseCast : Movement
+{
+    public Interactable focus;
+    private Camera cam;
+    
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray,out hit))
+            {
+                RemoveFocus();
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit,100))
+            {
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+    private void SetFocus(Interactable newFocus)
+    {
+        if (newFocus != focus)
+        {
+            if (focus !=null)
+                focus.OnDefocused();
+            
+            focus = newFocus; 
+        }
+        newFocus.OnFocused(transform);
+    }
+
+    private void RemoveFocus()
+    {
+        if (focus !=null)
+            focus.OnDefocused();
+        
+        focus = null;
+       
+    }
+}
