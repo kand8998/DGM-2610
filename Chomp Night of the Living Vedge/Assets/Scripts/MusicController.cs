7fa663430380ef7fa6f1e19bf4b292574a4c168d
Kaitlyn <kand8998@gmail.com>
2020-02-22 12:11:16 -0700

    Tutorial Assets Added

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/MusicController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    public GameAction Action;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        Action.action += StopMusic;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+}
