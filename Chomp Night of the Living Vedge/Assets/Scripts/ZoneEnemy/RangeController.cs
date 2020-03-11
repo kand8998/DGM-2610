a90ccfbdc5aea4863ea5fdae3ae975ab67929e02
Kaitlyn <kand8998@gmail.com>
2020-03-11 12:11:52 -0600

    Scripts Rewritten

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/ZoneEnemy/RangeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RangeController : MonoBehaviour
+{
+  public UnityEvent rangeEvent;
+  
+  public float radius = 10.0f;
+  private Transform target;
+
+  private void Start()
+  {
+    target = PlayerManager.instance.player.transform;
+  }
+
+  private void Update()
+  {
+    float distance = Vector3.Distance(target.position, transform.position);
+    
+    if (distance < radius)
+    {
+      rangeEvent.Invoke();
+    }
+  }
+  
+  private void OnDrawGizmosSelected()
+  {
+    Gizmos.color = Color.magenta;
+    Gizmos.DrawWireSphere(transform.position, radius);
+  }
+}
---
+++ Chomp Night of the Living Vedge/Assets/Scripts/ZoneEnemy/SliderController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class SliderController : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+    public void SetMaxHealth(int amount)
+    {
+        slider.maxValue = amount;
+        slider.value = amount;
+
+        fill.color = gradient.Evaluate(1f);
+        
+    }
+
+    public void SetHealth(int amount)
+    {
+        slider.value = amount;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
