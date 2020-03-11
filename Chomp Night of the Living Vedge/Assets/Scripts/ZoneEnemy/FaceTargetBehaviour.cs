1aaf3886593f2b0fe81fd700fdf675cad3c59931
Kaitlyn <kand8998@gmail.com>
2020-03-11 16:16:40 -0600

    Script to Face Player

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/ZoneEnemy/FaceTargetBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FaceTargetBehaviour : MonoBehaviour
+{
+    private Transform target;
+
+    private void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+    }
+
+    private void Update()
+    {
+        FaceTarget();
+    }
+
+    private void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation =
+            Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = lookRotation;
+    }
+}
