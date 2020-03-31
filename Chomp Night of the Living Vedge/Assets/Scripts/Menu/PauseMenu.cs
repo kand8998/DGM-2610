c962e4bd5415408e66d0f336ece9447a25ac794d
Kaitlyn <kand8998@gmail.com>
2020-03-30 20:35:37 -0600

    Pause Script Started

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseMenu : MonoBehaviour
+{
+    private static bool gameIsPaused = false;
+    public UnityEvent resumeEvent, pauseEvent;
+
+    private void ResumeGame()
+    {
+        resumeEvent.Invoke();
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void PauseGame()
+    {
+        pauseEvent.Invoke();
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
