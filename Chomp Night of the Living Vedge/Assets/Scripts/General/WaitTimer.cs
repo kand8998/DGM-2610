e113f903240166d173c375535c3aadc56c6098f7
Kaitlyn <kand8998@gmail.com>
2020-04-01 11:34:46 -0600

    Timer Script Adjusted

--- Chomp Night of the Living Vedge/Assets/Scripts/General/WaitTimer.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/General/WaitTimer.cs
@@ -1,7 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class WaitTimer : MonoBehaviour
 {
-    public bool on;
-    
+    public float waitTime;
+    public UnityEvent timerEvent;
+
+    public void StartTimer()
+    {
+        StartCoroutine(Wait());
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        timerEvent.Invoke();
+    }
 }
