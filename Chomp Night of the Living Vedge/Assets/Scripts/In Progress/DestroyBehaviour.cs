d1f12cf4ca63f135013ca17c0aed8d612e8d662a
Kaitlyn <kand8998@gmail.com>
2020-04-01 14:00:59 -0600

    Lighting Adjusted a Little

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/In Progress/DestroyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DestroyBehaviour : MonoBehaviour
+{
+    public UnityEvent beforeDestroyEvent, afterDestroyEvent;
+
+    public void DestroyThis ()
+    {
+        beforeDestroyEvent.Invoke();
+        Destroy(gameObject);
+
+        afterDestroyEvent.Invoke();
+        gameObject.SetActive(true);
+    }
+}
