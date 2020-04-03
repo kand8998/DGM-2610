1c186368b78158b6a2029688b35f3b9c0a3a5caa
Kaitlyn <kand8998@gmail.com>
2020-04-02 19:59:06 -0600

    Float Data Issue Adjusted

--- Chomp Night of the Living Vedge/Assets/Scripts/General/FloatData.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/General/FloatData.cs
@@ -14,13 +14,21 @@ public class FloatData : ScriptableObject
 
     public void UpdateValueLimitZero(float amount)
     {
-        if (value > 0 & value < 1)
+        if (value > 0)
         {
             UpdateValue(amount);
         }
-        else
+        if (value > 1)
+        {
+            value = 1;
+        }
+        if (value < 0)
         {
             value = 0;
+            
+        }
+        if (value == 0)
+        {
             zeroEvent.Invoke();
         }
     }
