56f2686ee4f3b04fc4e1650468adfda26660dfa8
Kaitlyn <kand8998@gmail.com>
2020-04-08 12:47:58 -0600

    ID System Added

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/ID Matching/ID's/DoWork.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DoWork : MonoBehaviour
+{
+    public NameID nameIdObj;
+    public UnityEvent workEvent;
+
+    public void Work()
+    {
+        workEvent.Invoke();
+    }
+}
---
+++ Chomp Night of the Living Vedge/Assets/Scripts/ID Matching/ID's/WorkSystemManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class WorkSystemManager : ScriptableObject
+{
+    [Serializable]
+    public struct WorkMatches
+    {
+        public NameID nameIdObj;
+        public WorkSystem workSystemObj;
+    }
+
+   
+}
