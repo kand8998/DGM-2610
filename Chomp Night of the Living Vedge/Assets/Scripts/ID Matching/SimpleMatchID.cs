0a0a8d317d7a4b73f4b27b75371f787a9c3452f2
Kaitlyn <kand8998@gmail.com>
2020-04-08 14:37:07 -0600

    ID Debugging
    Id debugging isn't working (trigger events = broken)

--- Chomp Night of the Living Vedge/Assets/Scripts/ID Matching/SimpleMatchID.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/ID Matching/SimpleMatchID.cs
@@ -5,17 +5,21 @@ public class SimpleMatchID : IDBehaviour
 {
     protected NameID OtherIDObj;
     public UnityEvent matchEvent, noMatchEvent;
-    public virtual void OnTriggerEnter(Collider other)
+    
+    public void OnTriggerEnter(Collider other)
     {
         OtherIDObj = other.GetComponent<IDBehaviour>().nameIDObj;
+       Debug.Log("The ID is triggering");
 
         if (nameIDObj == OtherIDObj)
         {
             matchEvent.Invoke();
+            Debug.Log("It's matching");
         }
         else
         {
             noMatchEvent.Invoke();
         }
     }
+ 
 }
---
+++ Chomp Night of the Living Vedge/Assets/Scripts/ID Matching/TriggerMatchID.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class TriggerMatchID : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggering Into..." + other.name);
+    }
+}
