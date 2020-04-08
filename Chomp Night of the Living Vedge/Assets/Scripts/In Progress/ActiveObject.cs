1401988db0cdc1b649c3c82ed2271434b2442fca
Kaitlyn <kand8998@gmail.com>
2020-04-08 12:08:59 -0600

    Set Active Script

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/In Progress/ActiveObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Tools/SetActive")]
+public class ActiveObject : ScriptableObject
+{
+
+    public void SetActiveTrue(GameObject obj)
+    {
+        obj.SetActive(true);
+    }
+
+    public void SetActiveFalse(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}
