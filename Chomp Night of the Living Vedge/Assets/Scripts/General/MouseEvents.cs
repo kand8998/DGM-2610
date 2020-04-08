218346f590a5644a552f1feed2f5c2b3c341715b
Kaitlyn <kand8998@gmail.com>
2020-04-08 11:43:12 -0600

    attack timer on mouse events

--- Chomp Night of the Living Vedge/Assets/Scripts/General/MouseEvents.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/General/MouseEvents.cs
@@ -1,17 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Events;
 
 public class MouseEvents : MonoBehaviour
 {
+    public float waitTime = 0.5f;
     public UnityEvent mousedownEvent, mouseUpEvent;
-
+    
+    private bool canUseMouse = true;
+    
     private void OnMouseDown()
     {
-        mousedownEvent.Invoke();
+        if (canUseMouse)
+        {
+            mousedownEvent.Invoke();
+        }
     }
 
     private void OnMouseUp()
     {
         mouseUpEvent.Invoke();
+        canUseMouse = false;
+        StartTimer();
+    }
+    
+    private void StartTimer()
+    {
+        StartCoroutine(Wait());
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        canUseMouse = true;
     }
 }
