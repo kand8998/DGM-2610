6e2981a5a5bd383741227b29b673fa0a67321ab1
Kaitlyn <kand8998@gmail.com>
2020-04-09 16:34:26 -0600

    Right Mouse Button Events Split

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/General/RightMouseButtonEvents.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RightMouseButtonEvents : MonoBehaviour
+{
+    public UnityEvent rightButtonDownEvent, rightButtonUpEvent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RightMouseDown();
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            RightMouseUp();
+        }
+    }
+    
+    private void RightMouseDown()
+    {
+        rightButtonDownEvent.Invoke();
+    }
+    private void RightMouseUp()
+    {
+        rightButtonUpEvent.Invoke();
+    }
+}
