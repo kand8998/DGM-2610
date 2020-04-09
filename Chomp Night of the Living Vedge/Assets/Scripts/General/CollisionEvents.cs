99fa12d11f681a9edb849b652a507ff86e7f8f35
Kaitlyn <kand8998@gmail.com>
2020-04-08 20:34:02 -0600

    Coins Placed in Well

--- Chomp Night of the Living Vedge/Assets/Scripts/General/CollisionEvents.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/General/CollisionEvents.cs
@@ -4,13 +4,10 @@ using UnityEngine.Events;
 public class CollisionEvents : MonoBehaviour
 {
     public UnityEvent collisionEvent, boolTrueEvent;
-    public BoolData switchData;
+    
     private void OnCollisionEnter(Collision other)
     {
-        if (switchData.Switch == true)
-        {
-            boolTrueEvent.Invoke();
-        }
         collisionEvent.Invoke();
+        Debug.Log("should be colliding");
     }
 }
