54701511a60c80c6b73cce6832e870b696305ecd
Kaitlyn <kand8998@gmail.com>
2020-04-13 12:15:03 -0600

    Vector 3 List Obj

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/In Progress/Vector3ListData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "List/Vector3ListData")]
+public class Vector3ListData : ScriptableObject
+{
+    public List<Vector3Data> Vector3ListObj;
+    public int currentLineNumber;
+
+    public Vector3Data ReturnCurrentLine()
+    {
+        return Vector3ListObj[currentLineNumber];
+    }
+
+    public void ResetToZero()
+    {
+        currentLineNumber = 0;
+    }
+
+    public void IncrementLineNumber()
+    {
+        if (currentLineNumber < Vector3ListObj.Count)
+        {
+            currentLineNumber++;
+        }
+        else
+        {
+            currentLineNumber = 0;
+        }
+    }
+}
