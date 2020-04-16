a97e8ac66f542079cab39518eb0b4a1ec1444e00
Kaitlyn <kand8998@gmail.com>
2020-04-15 20:08:53 -0600

    Working on location scripts

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/Restart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Restart : MonoBehaviour
+
+{
+    public Vector3Data location;
+
+    public void ReturnToSpawn()
+    {
+        transform.position = location.value;
+    }
+
+    public void UpdateCheckPoint(Vector3Data checkPoint)
+    {
+        checkPoint.value = location.value;
+    }
+
+}
