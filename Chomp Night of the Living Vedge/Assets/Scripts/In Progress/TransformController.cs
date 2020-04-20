b8bbc7d53c544f973d7683474e5b6dfe0c09ed89
Kaitlyn <kand8998@gmail.com>
2020-04-20 11:46:20 -0600

    Working on checkpoint behavior scripts

--- Chomp Night of the Living Vedge/Assets/Scripts/In Progress/TransformController.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/In Progress/TransformController.cs
@@ -2,8 +2,26 @@
 
 public class TransformController : MonoBehaviour
 {
+    private Vector3Data playerData;
+
+    private void Start()
+    {
+        playerData.value = playerData.value;
+    }
+
     public void SetTransform(Transform destination)
     {
-        this.transform.position = destination.position;
+        transform.position = destination.position;
+    }
+
+    public void SetTransform(Vector3Data destination)
+    {
+        transform.position = destination.value;
+    }
+
+    public void SetTransform(Vector3ListData listObj)
+    {
+        playerData.UpdateTransform(transform);
+        playerData = listObj.ReturnCurrentLine();
     }
 }
