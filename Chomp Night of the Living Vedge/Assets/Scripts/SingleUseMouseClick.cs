f7bae48734baf14898cd0d15777bdf22de5ed92f
Kaitlyn <kand8998@gmail.com>
2020-04-25 17:45:15 -0600

    Debugging issue

--- Chomp Night of the Living Vedge/Assets/Scripts/SingleUseMouseClick.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/SingleUseMouseClick.cs
@@ -8,13 +8,16 @@ public class SingleUseMouseClick : MonoBehaviour
 
   private void Start()
   {
+    Debug.Log("It's starting at least");
     usedBool = false;
   }
 
   private void OnMouseDown()
   {
+    Debug.Log("Registering Mouse");
     if (usedBool == false)
     {
+      Debug.Log("registering that it isn't used");
       UseEvent.Invoke();
       usedBool = true;
     }
