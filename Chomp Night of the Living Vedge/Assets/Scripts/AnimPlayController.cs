34e30fe5729fd4f2731b7784b98fb8529066d017
Kaitlyn <kand8998@gmail.com>
2020-04-29 13:19:54 -0600

    Script for animation

---
+++ Chomp Night of the Living Vedge/Assets/Scripts/AnimPlayController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AnimPlayController : MonoBehaviour
+{
+    public BoolData playBoolObj;
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        if (playBoolObj.Switch == true)
+        {
+            playBoolObj.Switch = false;
+        }
+
+        if (playBoolObj.Switch == false)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
