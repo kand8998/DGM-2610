28d2aebe652b936342adf8165c6de38d72a1f377
Kaitlyn <kand8998@gmail.com>
2020-04-29 17:02:24 -0600

    Adjusting surface errors

--- Chomp Night of the Living Vedge/Assets/Scripts/AnimPlayController.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/AnimPlayController.cs
@@ -3,16 +3,14 @@
 public class AnimPlayController : MonoBehaviour
 {
     public BoolData playBoolObj;
-    private Animator animator;
+    
     private void Awake()
     {
-        animator = GetComponent<Animator>();
         if (playBoolObj.Switch == true)
         {
             playBoolObj.Switch = false;
         }
-
-        if (playBoolObj.Switch == false)
+        else
         {
             gameObject.SetActive(false);
         }
