9fbc2cd4af8a09624a5d4a6d66a8d18b6c5c127c
Kaitlyn <kand8998@gmail.com>
2020-04-29 18:16:39 -0600

    Less than zero script

--- Chomp Night of the Living Vedge/Assets/Scripts/General/FloatData.cs
+++ Chomp Night of the Living Vedge/Assets/Scripts/General/FloatData.cs
@@ -40,11 +40,12 @@ public class FloatData : ScriptableObject
     
     public void UpdateValueLowerLimit(float amount)
     {
-        if (value > 0)
+        if (value >= 0)
         {
             UpdateValue(amount);
         }
-        else
+
+        if (value < 0 )
         {
             value = 0;
             zeroEvent.Invoke();
